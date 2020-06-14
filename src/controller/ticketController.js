88de4846332d36bea74aaa69283a6ea5a9115874
ariel <ariel.roque@hotmail.com>
2020-06-14 10:48:15 -0300

    Add endpoints

--- src/controller/ticketController.js
+++ src/controller/ticketController.js
@@ -12,6 +12,19 @@ module.exports = {
     },
 
     async addTicket(req,res){
+        const ticket = new Ticket({
+            title:req.body.title,
+            description:req.body.description,
+            priority:req.body.priority,
+            authorId:req.body.authorId,
+        });
+
+        ticket.save((err,docs)=>{
+            if(err){
+                res.send(`Error ${JSON.stringify(err,undefined,2)}`);
+            }
+            return res.send(docs);
+        });
 
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const routes = express.Router();
 const userController = require('./controller/userController');
+const ticketController = require('./controller/ticketController');
+const projectController = require('./controller/projectController');
 
-
+//User endpoints
 routes.post('/user',userController.addUser);
 routes.get('/user',userController.getUsers);
+
+//Ticket endpoints
+
+routes.post('/ticket',ticketController.addTicket);
+routes.get('/ticket',ticketController.getTickets);
+
+//Project endpoints
+routes.post('/project',projectController.addProject);
+routes.get('/project',projectController.getProjects);
 
 
 module.exports = routes;
